86ed57c5dd9b0eef2072023b135ec123bc3b4bba
Muhammad Rizwan <allgame45@hotmail.com>
2019-08-25 12:28:14 +0500

    Signed-off-by: Muhammad Rizwan <allgame45@hotmail.com>

--- db/3.sql
+++ db/3.sql
@@ -17,3 +17,15 @@ CREATE TABLE [dbo].[SubStock](
 ) ON [PRIMARY]
 
 GO
+
+CREATE TABLE [dbo].[wpacking](
+	[wpackingid] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nchar](255) NOT NULL,
+	[Active] [bit] NOT NULL,
+ CONSTRAINT [PK_wpacking] PRIMARY KEY CLUSTERED 
+(
+	[wpackingid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
