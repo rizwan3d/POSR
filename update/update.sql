8886ca00a5d5b6249df298ef27722c4ff955061e
Muhammad Rizwan <allgame45@hotmail.com>
2020-04-21 00:15:21 +0500

    Signed-off-by: Muhammad Rizwan <allgame45@hotmail.com>

--- update/update.sql
+++ update/update.sql
@@ -1,6 +1,102 @@
 USE [DDPOSDB]
 GO
 
+CREATE TABLE [dbo].[SubStock](
+	[SubStockid] [int] IDENTITY(1,1) NOT NULL,
+	[GroupName] [nchar](255) NULL,
+	[ProductId] [money] NULL,
+	[Stock] [money] NULL,
+	[SalePrice] [money] NULL,
+	[PurchasePrice] [money] NULL,
+	[ExpireDate] [date] NULL,
+	[another] [nchar](100) NULL,
+ CONSTRAINT [PK_SubStock] PRIMARY KEY CLUSTERED 
+(
+	[SubStockid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+CREATE TABLE [dbo].[wpacking](
+	[wpackingid] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nchar](255) NOT NULL,
+	[Active] [bit] NOT NULL,
+ CONSTRAINT [PK_wpacking] PRIMARY KEY CLUSTERED 
+(
+	[wpackingid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+CREATE TABLE [dbo].[JournalVoucher](
+	[JournalVoucherId] [int] IDENTITY(1,1) NOT NULL,
+	[date] [date] NULL,
+	[Description] [nchar](255) NULL,
+ CONSTRAINT [PK_JournalVoucher] PRIMARY KEY CLUSTERED 
+(
+	[JournalVoucherId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+CREATE TABLE [dbo].[JvItems](
+	[JvItemId] [int] IDENTITY(1,1) NOT NULL,
+	[AccountId] [int] NOT NULL,
+	[Dis] [nchar](255) NULL,
+	[Dr] [money] NULL,
+	[Cr] [money] NULL,
+	[JournalVoucherId] [int] NOT NULL,
+ CONSTRAINT [PK_JvItems] PRIMARY KEY CLUSTERED 
+(
+	[JvItemId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[JvItems]  WITH CHECK ADD  CONSTRAINT [FK_JvItems_Accounts] FOREIGN KEY([AccountId])
+REFERENCES [dbo].[Accounts] ([AccountId])
+GO
+
+ALTER TABLE [dbo].[JvItems] CHECK CONSTRAINT [FK_JvItems_Accounts]
+GO
+
+ALTER TABLE [dbo].[JvItems]  WITH CHECK ADD  CONSTRAINT [FK_JvItems_JournalVoucher] FOREIGN KEY([JournalVoucherId])
+REFERENCES [dbo].[JournalVoucher] ([JournalVoucherId])
+GO
+
+ALTER TABLE [dbo].[JvItems] CHECK CONSTRAINT [FK_JvItems_JournalVoucher]
+
+GO
+
+CREATE TABLE [dbo].[Salesman](
+	[SalesmanId] [int] IDENTITY(1,1) NOT NULL,
+	[AccountId] [int] NOT NULL,
+	[Commission] [money] NULL,
+	[Salary] [money] NULL,
+	[Active] [bit] NULL,
+ CONSTRAINT [PK_Salesman] PRIMARY KEY CLUSTERED 
+(
+	[SalesmanId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[Salesman] ADD  CONSTRAINT [DF_Salesman_Active]  DEFAULT ((1)) FOR [Active]
+GO
+
+GO
+IF COL_LENGTH('SaleInvoice', 'SaleMen') IS NULL
+BEGIN
+    ALTER TABLE SaleInvoice
+    ADD [SaleMen] [int]
+END
+
+GO
+
 IF COL_LENGTH('Products', 'txtextraurdu') IS NULL
 BEGIN
     ALTER TABLE Products
@@ -114,12 +210,15 @@ GO
 IF COL_LENGTH('Ledger', 'created_at') IS NULL
 BEGIN
     ALTER TABLE Ledger
-    ADD [created_at] [datetime]
+    ADD [created_at] [datetime];
 END
 
 Go
 
 ALTER TABLE [dbo].[Ledger] ADD  CONSTRAINT [DF_Ledger_created_at]  DEFAULT (getdate()) FOR [created_at]
+GO
+
+ALTER TABLE SaleInvoice  ALTER COLUMN Customer NVARCHAR(255) NOT NULL; 
 GO
 
 IF COL_LENGTH('PurchasesInvoice', 'Dis') IS NULL
@@ -130,14 +229,6 @@ END
 
 Go
 
-ALTER TABLE SaleInvoice ALTER COLUMN Customer VARCHAR (100);
 
-Go
-
-IF COL_LENGTH('SaleInvoice', 'OrderType') IS NULL
-BEGIN
-    ALTER TABLE SaleInvoice
-    ADD [OrderType] [nchar](50) NULL
-END
-
+INSERT [dbo].[ResTableInfo] ( [TableName],[Active]) VALUES ( N'NULL',1)
 Go
